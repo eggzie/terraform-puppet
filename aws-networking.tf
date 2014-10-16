0e2fce3b205174ac7c370aab98ce8bed00a863e0
Tim Hartmann <tfhartmann@gmail.com>
2014-10-16 14:04:58 -0400

    Update to networking

--- aws-networking.tf
+++ aws-networking.tf
@@ -1,6 +1,7 @@
 # VPC
 resource "aws_vpc" "tf-puppet" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block           = "10.0.0.0/16"
+    enable_dns_hostnames = "true"
 }
 resource "aws_internet_gateway" "gw" {
     vpc_id = "${aws_vpc.tf-puppet.id}"
